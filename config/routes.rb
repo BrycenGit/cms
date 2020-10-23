8093c8eaccb395a0dbfe3c8a34b1e0a374e3fc0b
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-23 10:00:47 -0700

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/show'
-  get 'comments/edit'
-  get 'bios/new'
-  get 'bios/show'
-  get 'bios/edit'
-  get 'skills/new'
-  get 'skills/show'
-  get 'skills/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#index"
   resources :users do
     resources :projects
+  end
+  resources :users do
+    resources :posts
+  end
+  resources :users do
+    resources :skills
+  end
+  resources :users do
+    resources :bio
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_162224) do
+ActiveRecord::Schema.define(version: 2020_10_23_164428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_10_23_162224) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comment_posts", force: :cascade do |t|
+    t.bigint "comment_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_comment_posts_on_comment_id"
+    t.index ["post_id"], name: "index_comment_posts_on_post_id"
   end
 
   create_table "comment_projects", force: :cascade do |t|
@@ -49,6 +58,21 @@ ActiveRecord::Schema.define(version: 2020_10_23_162224) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_users", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_users_on_post_id"
+    t.index ["user_id"], name: "index_post_users_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
